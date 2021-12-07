2d1fb15c483f72afe0d097f0723779b2cab19bd8
yangmingjie <1519284616@qq.com>
2021-12-07 19:12:00 +0800

    完成了主页功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,7 +1,27 @@
-// 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
-// 会先调用 ajaxPrefilter 这个函数
-// 在这个函数中，可以拿到我们给Ajax提供的配置对象
-$.ajaxPrefilter(function(options) {
-  // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-  options.url = 'http://ajax.frontend.itheima.net' + options.url
-})
+$.ajaxPrefilter( function( options ) { 
+    options.url = 'http://api-breakingnews-web.itheima.net'+options.url; 
+    // 统一为用权限的接口设置请求头
+    // 判断路径是否有/my这个字符串，如果有这个字符串说明是有权限的接口
+    if(options.url.indexOf('/my/')!==-1){
+      options.headers={
+          Authorization:localStorage.getItem('token') || ""
+
+      }
+
+    
+    // 全局配置complete
+    options.complete=function(res){
+        
+        // 返回值中的responseJSON就是服务器响应回来的数据
+        console.log(res.responseJSON);
+        // 
+        if(res.responseJSON.status===1){
+            // 清除本地token
+            localStorage.removeItem('token');
+            // 跳转到登录页
+            location.href="./login.html"
+        }
+    }
+  }
+    
+  });
---
+++ assets/js/index.js
@@ -0,0 +1,60 @@
+$(function(){
+    
+    // 调用函数获取用户的基本信息
+    getUserInfo();
+    
+    $('#logOut').on('click',function(){
+        console.log(1);
+        layer.confirm('确定退出登录？', {icon: 3, title:'提示'}, function(index){
+            //do something
+            // 1、清空本地存储中的token
+            localStorage.removeItem('token');
+            // 2、跳转到登录页
+            location.href="./login.html";
+            layer.close(index);
+          });
+    })
+    
+})
+var layer = layui.layer;
+// 获取用户的基本信息
+function getUserInfo(){
+    $.ajax({
+        method:'GET',
+        url:'/my/userinfo',
+        // 请求头配置对象
+        /* headers:{
+            Authorization:localStorage.getItem('token') || ""
+        }, */
+        success:function(res){
+            if(res.status!==0){
+                return layer.msg('获取用户信息失败')
+            }
+            // 调用函数渲染头像
+            renderAvatar(res.data);
+            
+        },
+        
+    })
+}
+// 渲染用户头像,将用户信息传递给这个函数
+function renderAvatar(user){
+    // 欢迎后面的名字
+    // 如果用户有昵称就渲染昵称，如果没有就渲染用户名
+    var welcomeName = user.nickname||user.username;
+    $('#welcome-name').append(user.username)
+    // 判断用户有没有头像图片
+    if(user.user_pic===null){
+        // 用户没有头像图片,渲染文本头像
+        var namePic = welcomeName[0].toUpperCase();
+        $('.text-avatar').html(namePic);
+        $('.layui-nav-img').hide();
+    }else{
+        // 渲染图片头像
+        $('.text-avatar').hide();
+        $('.layui-nav-img').show();
+        $('.layui-nav-img').attr('src',user.user_pic).show();
+    }
+    // 等渲染好内容再显示到页面上
+    $('.userinfo').show();
+}
